58f97fb95e7bf596892115112798802d4be479a3
Antti Kupila <akupila@gmail.com>
2013-01-20 14:12:00 +0100

    Added support for atmega8 and atmega128 (not tested)

--- ContinuousServo.cpp
+++ ContinuousServo.cpp
@@ -101,8 +101,14 @@ void ContinuousServo::initializeTimer()
 	TCNT1 	= 0;				// zero out 16bit counter register
 	TCCR1B	= _BV(CS11);		// start timer with /8 prescaler
 
-	TIFR1	= _BV(OCF1A);	 	// clear any pending interrupts; 
-	TIMSK1	= _BV(OCIE1A);		// enable the output compare interrupt
+#if defined(__AVR_ATmega8__)|| defined(__AVR_ATmega128__)
+	TIFR |= _BV(OCF1A);      // clear any pending interrupts; 
+	TIMSK |=  _BV(OCIE1A) ;  // enable the output compare interrupt  
+#else
+	// here if not ATmega8 or ATmega128
+	TIFR1 |= _BV(OCF1A);     // clear any pending interrupts; 
+	TIMSK1 |=  _BV(OCIE1A) ; // enable the output compare interrupt 
+#endif
 	timerInitialized = true;
 }
 
